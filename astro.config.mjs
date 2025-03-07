7044d4b1f2cbc1eb1e71d67d83803954bf1d68f4
JulioDev <juliolh06@gmail.com>
2025-03-07 00:10:20 -0500

    Your commit message

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,25 +2,27 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 
-import react from '@astrojs/react';
+import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
+  base: "https://github.com/juliolh0686/portfolio",
+  outDir: "dist",
   integrations: [tailwind(), react()],
   vite: {
     resolve: {
       alias: {
-        '@': '/src',
-        '@components': '/src/components'
-      }
-    }
+        "@": "/src",
+        "@components": "/src/components",
+      },
+    },
   },
-  output: 'static',
+  output: "static",
   build: {
-    inlineStylesheets: 'auto'
+    inlineStylesheets: "auto",
   },
   server: {
     host: true,
     port: 4321,
-  }
+  },
 });
